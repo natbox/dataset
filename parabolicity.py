3fed1b94c46729b450a9095267f68aefe349409c
natbox <julian.kiesenhofer@outlook.at>
2024-08-28 13:14:23 +0200

    Add files via upload

---
+++ parabolicity.py
@@ -0,0 +1,17 @@
+r=10
+z=0
+d='.'
+b=' '
+c=lambda z:r/z
+h='*'
+print((int(c(0.08))+5)*h)
+for x in range(int(c(0.5))):
+ u=str(h+3*((2*r-x)*b+d))
+ v=str(h+3*(2*x*b)+h)
+ w=str(3*(d+(2*r-x)*b)+h)
+ print(u+v+w)
+s1=(int(c(0.16))+2)*h
+s2=str(len(u+v+w)-2)
+s3=(int(c(0.16))+1)*h
+cc=s1+s2+s3
+print(cc)
