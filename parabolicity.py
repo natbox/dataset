bce07838ee63fbbd60cf0e462a3a3c3582f1dd35
natbox <julian.kiesenhofer@outlook.at>
2024-11-28 22:27:17 +0100

    Update parabolicity.py

--- parabolicity.py
+++ parabolicity.py
@@ -25,3 +25,24 @@ class Dashes:
 dash=Dashes()
 s3=(int(c(0.16))+1)*h
 cc=s1+dash.s2+s3
+
+class Points:
+  def __init__(self):
+    print((int(c(0.08))+5)*h)
+  def __call__(self):
+    print(cc)
+a = Points()
+
+for x in range(int(c(0.5))):
+  u=str(h+3*((2*r-x)*b+d))
+  v=str(h+3*(2*x*b)+h)
+  w=str(3*(d+(2*r-x)*b)+h)
+  sum=u+v+w
+  print(sum)
+
+s1=(int(c(0.16))+2)*h
+class Dashes:
+  s2=str(len(u+v+w)-2)
+dash=Dashes()
+s3=(int(c(0.16))+1)*h
+cc=s1+dash.s2+s3
