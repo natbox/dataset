88afd1332be3a5a54a2e68e9ffbe99bd17d36da4
natbox <julian.kiesenhofer@outlook.at>
2024-11-28 19:47:35 +0100

    Create convexity.f90

---
+++ convexity.f90
@@ -0,0 +1,31 @@
+module samplehub
+   contains
+   function my(port)
+   real, dimension(:) :: port
+   real, dimension(size(port)) :: a
+   end function my
+end module samplehub
+program main
+    use samplehub, only : my
+    implicit none
+    character(len=*), parameter :: OUT_FILE = 'inner.txt'
+    character(len=*), parameter :: PLT_FILE = 'microport.plt'
+    integer,          parameter :: j        =  14
+    real, dimension(256) :: series
+    real, dimension(384) :: outer
+    integer :: u, carbon
+    real    :: x(j), y(j)
+    x(1) = 0.0
+    y(1) = 264
+    do u = 2, j
+        x(u) = 0.1 * u * 8
+        y(u) = 264-x(u)**2
+    end do
+    open (action='write', file=OUT_FILE, newunit=carbon, status='replace')
+    do u = 1, j
+        write (carbon, *) x(u), y(u)
+    end do
+    close (carbon)
+    call execute_command_line('gnuplot -p ' // PLT_FILE)
+    guess = my(series)
+end program main
