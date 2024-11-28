24c2e71add0e7c1c135ec3ef9d83f8a021b148bd
natbox <julian.kiesenhofer@outlook.at>
2024-11-28 20:46:09 +0100

    Update parabolicity.py

--- parabolicity.py
+++ parabolicity.py
@@ -5,10 +5,6 @@ b=' '
 c=lambda z:r/z
 h='*'
 #print((int(c(0.08))+5)*h)
-s1=(int(c(0.16))+2)*h
-s2=str(len(u+v+w)-2)
-s3=(int(c(0.16))+1)*h
-cc=s1+s2+s3
 #print(cc)
 class Points:
   def __init__(self):
@@ -21,4 +17,8 @@ for x in range(int(c(0.5))):
  v=str(h+3*(2*x*b)+h)
  w=str(3*(d+(2*r-x)*b)+h)
  print(u+v+w)
+s1=(int(c(0.16))+2)*h
+s2=str(len(u+v+w)-2)
+s3=(int(c(0.16))+1)*h
+cc=s1+s2+s3
 a()
