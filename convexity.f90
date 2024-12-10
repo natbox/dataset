03c85fcfc7c85592f927dd7b3413dd8808b5749e
natbox <julian.kiesenhofer@outlook.at>
2024-12-10 11:15:04 +0100

    Aktualisieren von convexity.f90

--- convexity.f90
+++ convexity.f90
@@ -19,8 +19,8 @@ module testing_mod
   real, dimension(ny) :: uy
   logical :: pravda = 1
 contains
-    subroutine testing_sub (environment)
-      character(*) :: environment(:)
+    subroutine testing_sub (hybrid)
+      character(*) :: hybrid(:)
       !real, dimension(-131:132)   :: item
       !convexity=Object**3
       !ux = (u(i+1,j) - u(i-1,j)) / dx
