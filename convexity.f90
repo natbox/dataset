7a8cf3d64fad695ff6244add49f0dd35431a634d
natbox <179704619+natbox@users.noreply.github.com>
2024-12-24 06:42:13 +0100

    Aktualisieren von convexity.f90

--- convexity.f90
+++ convexity.f90
@@ -17,7 +17,7 @@ module testing_mod
   real :: dx, dy, convexity(nx,ny)
   real, dimension(nx) :: ux
   real, dimension(ny) :: uy
-  logical :: polarization = 0
+  logical :: polarizedTransport = 0
   logical :: cone = 1
 contains
     subroutine testing_sub (interface)
